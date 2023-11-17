dc48496e09908df0874a192689ec497bf7e4b227
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-11-17 03:06:53 +0000

    added letter_opnener gem and config lines

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,6 +69,8 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'projects.local', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web 
+
   config.hosts << "projects.local:3000"
 end
 
